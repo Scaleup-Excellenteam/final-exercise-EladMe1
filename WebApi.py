d7f21534e35e0fc3dc0a656912c30ed40dd165ea
Elad Menahem <93273498+EladMe1@users.noreply.github.com>
2023-06-09 13:20:23 +0300

    handle status check

--- WebApi.py
+++ WebApi.py
@@ -1,8 +1,9 @@
+import json
 import os
-
+import re
 import uuid
-from datetime import datetime
 
+from datetime import datetime
 from flask import Flask, render_template, request, jsonify
 
 
@@ -17,7 +18,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/upload",methods=["POST"])
+@app.route("/upload", methods=["POST"])
 def upload():
     try:
         if "file" not in request.files:
@@ -49,6 +50,94 @@ def upload():
             return jsonify({"uid": uid}), 200
     except Exception as e:
         return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+
+
+@app.route("/check_file", methods=["GET", "POST"])
+def check_file():
+    try:
+        uid = request.args.get("uid")
+        if uid:
+            done_success, done_data = is_done(uid)
+
+            if done_success:
+                return jsonify(done_data), 200
+
+            pending_success, pending_data = is_pending(uid)
+            if pending_success:
+                return jsonify(pending_data), 200
+
+        return jsonify({"message": f"No file with UID {uid} found."}), 404
+
+    except Exception as e:
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+
+
+def is_done(uid):
+    files = os.listdir('outputs')
+    for file in files:
+        if uid in file:
+            original_filename = extract_filename(file)
+            timestamp = extract_timestamp(file)
+            explanation = get_explanation(uid)
+            return True, {
+                "status": "done",
+                "filename": original_filename,
+                "timestamp": timestamp,
+                "explanation": explanation
+            }
+    return False, {}  # File not found, return False and an empty dictionary
+
+
+def get_explanation(uid):
+    files = os.listdir('outputs')
+    for file in files:
+        if uid in file:
+            with open(os.path.join('outputs', file), 'r') as f:
+                json_data = f.read()
+                data = json.loads(json_data)
+                return data
+    return None
+
+
+def extract_filename(file):
+    filename_parts = file.split("_", 1)
+    original_filename = filename_parts[0]
+    return original_filename
+
+
+def extract_timestamp(file):
+    timestamp_match = re.search(r"_(\d{14})", file)
+    timestamp = ""
+    if timestamp_match:
+        timestamp = timestamp_match.group(1)
+    return format_timestamp(timestamp)
+
+
+def is_pending(uid):
+    files = os.listdir('pending')
+    for file in files:
+        if uid in file:
+            original_filename = extract_filename(file)
+            timestamp = extract_timestamp(file)
+            return True, {
+                "status": "pending",
+                "filename": original_filename,
+                "timestamp": timestamp,
+                "explanation": "None"
+            }
+    return False, {}
+
+
+def format_timestamp(timestamp):
+    year = timestamp[:4]
+    month = timestamp[4:6]
+    day = timestamp[6:8]
+    hour = timestamp[8:10]
+    minute = timestamp[10:12]
+    second = timestamp[12:14]
+
+    formatted_timestamp = f"Upload Date: {day}/{month}/{year} at {hour}:{minute}:{second}"
+    return formatted_timestamp
 
 
 def allowed_file(filename):
